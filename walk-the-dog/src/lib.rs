de6af77c5e8ed45b9b266891e162776ee56e8e8f
naoki85 <naoki.yoneyama.85@gmail.com>
2025-04-06 10:59:16 +0900

    game development with rust and wasm chapter 3

--- walk-the-dog/src/lib.rs
+++ walk-the-dog/src/lib.rs
@@ -1,6 +1,12 @@
+#[macro_use]
+mod browser;
+mod engine;
+mod game;
+
+use engine::GameLoop;
+use game::WalkTheDog;
 use wasm_bindgen::prelude::*;
-use std::{rc::Rc, sync::Mutex, collections::HashMap};
-use serde::Deserialize;
+use anyhow::Result;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -16,87 +22,10 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
-
-    let context = canvas.get_context("2d").unwrap().unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
-
-    wasm_bindgen_futures::spawn_local(async move {
-        let json = fetch_json("rhb.json").await.expect("Could not fetch rhb.json");
-        let sheet: Sheet = serde_wasm_bindgen::from_value(json).expect("Could not convert rhb.json into a Sheet structure");
-
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-
-        let image = web_sys::HtmlImageElement::new().unwrap();
-
-        let callback = Closure::once(move || {
-            success_tx.lock().ok().and_then(|mut opt| opt.take());
-        });
-
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                let _ = error_tx.send(Err(err));
-            }
-        });
-
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-
-        image.set_src("rhb.png");
-        let _ = success_rx.await;
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            let _ = context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
-        }) as Box<dyn FnMut()>);
-        let _ = window.set_interval_with_callback_and_timeout_and_arguments_0(interval_callback.as_ref().unchecked_ref(), 50,);
-        interval_callback.forget();
+    browser::spawn_local(async move {
+        let game = WalkTheDog::new();
+        GameLoop::start(game).await.expect("Could not start game loop");
     });
 
     Ok(())
-}
-
-async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
-    let window = web_sys::window().unwrap();
-    let resp_value = wasm_bindgen_futures::JsFuture::from(
-        window.fetch_with_str(json_path)
-    ).await?;
-    let resp: web_sys::Response = resp_value.dyn_into()?;
-
-    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
-}
-
-#[derive(Deserialize)]
-struct Sheet {
-    frames: HashMap<String, Cell>,
-}
-
-#[derive(Deserialize)]
-struct Rect {
-    x: u16,
-    y: u16,
-    w: u16,
-    h: u16,
-}
-
-#[derive(Deserialize)]
-struct Cell {
-    frame: Rect,
 }
